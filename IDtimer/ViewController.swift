d0b83d07f21db662e019658799ad3e4f9a2c2609
2thlesss <thomas.jadie.reeves@gmail.com>
2023-03-05 21:33:32 -0500

    Initial Commit

---
+++ IDtimer/ViewController.swift
@@ -0,0 +1,76 @@
+//
+//  ViewController.swift
+//  IDtimer
+//
+//  Created by Thomas Jadie Reeves on 3/5/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var timerLabel: UILabel!
+    
+    @IBOutlet weak var startButtonTapped: UIButton!
+    
+    
+    @IBOutlet weak var roundLabel: UILabel!
+    
+    
+    var remainingTime = 0
+       var currentRound = 0
+       var roundDurations = [65, 65, 65, 65, 65, 65, 65, 65, 65, 65, 65, 65, 65, 65, 105, 105, 105, 105, 105, 105, 105, 105, 105, 125, 125]
+       var timer: Timer?
+       
+       override func viewDidLoad() {
+           super.viewDidLoad()
+       }
+       
+       override func viewWillDisappear(_ animated: Bool) {
+           super.viewWillDisappear(animated)
+           
+           timer?.invalidate()
+           timer = nil
+       }
+    
+    
+    @IBAction func startButtonTapped(_ sender: UIButton) {
+        // disable the button
+               startButtonTapped.isEnabled = false
+               
+               // check if timer is running, if yes, invalidate it
+               if timer != nil {
+                   timer?.invalidate()
+                   timer = nil
+               }
+               
+               currentRound = 1
+               remainingTime = roundDurations[currentRound-1] + 1 // add 1 second to the remaining time to start from x
+               timerLabel.text = "\(remainingTime)"
+               roundLabel.text = "Round \(currentRound)"
+               
+               timer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { [weak self] timer in
+                   guard let self = self else { return }
+                   
+                   if self.remainingTime > 0 { // check if remaining time is greater than 0
+                       self.remainingTime -= 1
+                       self.timerLabel.text = "\(self.remainingTime)"
+                   } else {
+                       self.currentRound += 1
+                       
+                       if self.currentRound > self.roundDurations.count {
+                           timer.invalidate()
+                           self.timer = nil
+                           self.timerLabel.text = "Done!"
+                           self.startButtonTapped.isEnabled = true // enable the button
+                       } else {
+                           self.remainingTime = self.roundDurations[self.currentRound-1] + 1 // add 1 second to the remaining time to start from x
+                           self.timerLabel.text = "\(self.remainingTime)"
+                           self.roundLabel.text = "Round \(self.currentRound)"
+                       }
+                   }
+               }
+           }
+       }
+/// now i need to put in the code to display the banner alerts and display in a lable the round information for buffs and towers
+/// 
